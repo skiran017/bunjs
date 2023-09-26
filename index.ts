880c9f763b3e48bdb89f265d94931f357c35596f
skiran017 <skiran017@gmail.com>
2023-09-26 11:03:35 +0530

    feat: basic server routing (api)

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 const server = Bun.serve({
   port: process.env.PORT || 5000,
   fetch(req) {
-    return new Response("Hello World");
+    const url = new URL(req.url);
+    if (url.pathname === '/') {
+      return new Response('HomePage!')
+    };
+    if (url.pathname === '/about') {
+      return new Response('AboutPage!')
+    };
+    return new Response('404! Page not Found')
   },
 })
 
